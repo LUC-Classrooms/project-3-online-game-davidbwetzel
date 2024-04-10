8b56bc4d038b198708c626c48bb46d9f0ddcbca9
davidbwetzel <david@wetzel-soyster.net>
2024-04-10 16:06:10 +0000

    Player 1 movement part 2 - arrow keys

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function play() {
   textAlign(CENTER);
   textSize(16);
   //text("This is where the Game happens", width / 2, height / 2);
-  player1.x = mouseX;
+  //player1.x = mouseX;
   player1.display();
 }
 
@@ -72,10 +72,49 @@ function mousePressed() {
   if(gameState == "splash"){
     gameState = "play";
   } else if(gameState == "play"){
-    gameState = "gameOver";
+    //gameState = "gameOver";
   } else if(gameState == "gameOver"){
     gameState = "splash";
   }
   console.log(gameState);
 
+}
+
+function keyPressed(){
+  switch(keyCode){
+    case UP_ARROW :
+      console.log("up");
+      player1.y -= 30 // move up 30px
+      player1.angle = 0; // no rotation
+      if(player1.y < 0) {
+        player1.y = height; 
+      }// wrap to bottom
+      break;
+    case DOWN_ARROW :
+      console.log('down');
+      player1.y += 30 // move up 30px
+      player1.angle = PI; //flip 180 degrees
+      if(player1.y > height) {
+        player1.y = 0; 
+      }// wrap to top
+      break;
+    case LEFT_ARROW :
+      console.log("left");
+      player1.x -=30;
+      player1.angle = PI + HALF_PI;
+      if(player1.x < 0){
+        player1.x = width;
+      }
+      break;
+    case RIGHT_ARROW :
+      console.log("right");
+      player1.x += 30;
+      player1.angle = HALF_PI;
+      if(player1.x > width){
+        player1.x = 0;
+      }
+      break;
+    default :
+      console.log("use the arrow keys to move")
+  }
 }
