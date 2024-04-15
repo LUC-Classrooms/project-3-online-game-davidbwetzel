fd9eb0b628e31fc043bf223a08792be4180aea65
davidbwetzel <david@wetzel-soyster.net>
2024-04-15 16:02:16 +0000

    Version 2 - Timer

--- script.js
+++ script.js
@@ -6,12 +6,14 @@
  */
 var gameState = "splash";
 var player1;
+var gameTimer; // time the game play
 
 function setup() {
 
   createCanvas(600, 400);
   player1 = new Player(width/2, height * 4/5);
   console.log(player1);
+  gameTimer = new Timer(10000); // 10 seconds for now
 
 }
 
@@ -48,7 +50,7 @@ function splash() {
 
 function play() {
   // this is what you see when the game is running 
-  background(0, 200, 0);
+  background(0, 200, 0); // green
   fill(0, 0, 200)
   textAlign(CENTER);
   textSize(16);
@@ -56,6 +58,12 @@ function play() {
   //player1.x = mouseX;
   player1.display();
   player1.move();
+  if(gameTimer.isFinished()){
+    gameState = "gameOver";
+  }
+  textAlign(LEFT);
+  text("elapsed time: " + gameTimer.elapsedTime, 40, 100);
+  // show elapsed time in top left corner
 
   if(keyIsPressed) {
     switch(keyCode)
@@ -93,6 +101,7 @@ function mousePressed() {
   // console.log("click!");
   if(gameState == "splash"){
     gameState = "play";
+    gameTimer.start(); // start the timer
   } else if(gameState == "play"){
     //gameState = "gameOver";
   } else if(gameState == "gameOver"){
